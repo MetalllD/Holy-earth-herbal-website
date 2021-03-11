7dd897b54878f9f8f6fa36ec383bc6412020bb3e
mitalkamani <45828315+wocmit@users.noreply.github.com>
2021-03-11 22:48:05 +0530

    Added scroll animations

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,33 +3,39 @@ import Logo from './logo-line';
 import ProdSect from './product-sect';
 import bowls from '../../assets/bowls.png';
 import leaf from '../../assets/leaf.png';
+import Fade from 'react-reveal/Fade';
 
 
 function Home(){
     return(
       <div className="Home">
         <div className="hero">
+        <Fade bottom>
         <div id="title">
             <text>
                 <h1>Beautifully crafted handmade natural products</h1>
                 <h6>Beautifully crafted handmade natural products made from 100 percent natural products </h6>
             </text>
+        </div>
+        </Fade>
+                    <div className="hero-section">
+                    <Fade bottom>
+                        <div id="leaf">
+                          <img
+                              src={leaf}
+                              alt="bowls"
+                        />
+                        </div>
+                    </Fade>
+                    <Fade bottom>
+                        <div id="bowls">
+                          <img
+                              src={bowls}
+                              alt="bowls"
+                          />
+                        </div>
+                    </Fade>
           </div>
-          <div className="hero-section">
-              <div id="leaf">
-                <img
-                    src={leaf}
-                    alt="bowls"
-                />
-              </div>
-              <div id="bowls">
-                <img
-                    src={bowls}
-                    alt="bowls"
-                />
-             </div>
-          </div>
-  
         </div>
           
 
